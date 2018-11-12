bc01f5213a911b9ee013efdecf1209d341091f6a
hantangtouzi <hantangtouzi@foxmail.com>
2018-11-12 10:21:50 +0800

    void

--- security-helloworld/src/main/java/com/hantangtouzi/security/helloworld/SecurityHelloWorldApplication.java
+++ security-helloworld/src/main/java/com/hantangtouzi/security/helloworld/SecurityHelloWorldApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class SecurityHelloWorldApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(SecurityHelloWorldApplication.class, args);
     }
 }
