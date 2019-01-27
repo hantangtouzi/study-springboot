fbb924392e6e43ebfcdd260150d289dc435c6cca
hantangtouzi <hantangtouzi@foxmail.com>
2019-01-28 06:56:03 +0800

    void

---
+++ security-contacts/src/main/java/com/hantangtouzi/securitycontacts/SecurityContactsApplication.java
@@ -0,0 +1,15 @@
+package com.hantangtouzi.securitycontacts;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author WilliamChang.
+ * Created on 2019-01-28 06:43:40
+ */
+
+public class SecurityContactsApplication {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SecurityContactsApplication.class);
+        springApplication.run(args);
+    }
+}
